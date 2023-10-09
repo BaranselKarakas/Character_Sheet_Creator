6f0d1a6fba9d5e4f8254b6686b84f83bbfefa09a
Baransel Karakas <karakas@synyx.de>
2023-10-09 10:07:02 +0200

    Initial commit
    files were missing in commit before

---
+++ src/main/java/com/baran/charactersheetcreator/CharacterSheetCreatorApplication.java
@@ -0,0 +1,14 @@
+package com.baran.charactersheetcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class CharacterSheetCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterSheetCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baran/charactersheetcreator/CharacterSheetCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baran.charactersheetcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterSheetCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
