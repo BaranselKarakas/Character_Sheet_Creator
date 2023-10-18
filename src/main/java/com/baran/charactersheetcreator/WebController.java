3b416cbff55378e50cb6c3f76242a29e5f67c090
BaranselKarakas <baransel@live.de>
2023-10-18 22:03:42 +0200

    add basic functionality to webapp
    now can submit "name" and get it viewed on characterpage

--- src/main/java/com/baran/charactersheetcreator/WebController.java
+++ src/main/java/com/baran/charactersheetcreator/WebController.java
@@ -1,29 +1,36 @@
 package com.baran.charactersheetcreator;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class WebController {
-    @GetMapping(value = "/Hello")
+    @GetMapping("/Hello")
     @ResponseBody
     public String getRoot() {
         return "Hello World";
     }
 
-    @GetMapping(value = "/")
-    public String getCharacterCreator() {
+    @GetMapping("/")
+    public String getCharacterCreator(Model model) {
+        model.addAttribute("character", new Character());
         return "charactercreator";
     }
 
     @PostMapping("/")
-    @ResponseBody
-    public void saveCharacter(@RequestParam("name") String name) {
-        CharacterCreator characterCreator = new CharacterCreator();
-        characterCreator.setName(name);
+    public String submitCharacter(@ModelAttribute Character character, Model model) {
+        model.addAttribute("character", character);
+        return "characterpage";
+    }
+    @GetMapping("/character")
+    public String characterPage(Model model) {
+        return "characterpage";
     }
 
 }
