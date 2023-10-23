ed774b96ebde721288ab96b066a1c0454dcb1f00
Baransel Karakas <karakas@synyx.de>
2023-10-23 16:13:59 +0200

    add characterListPage() + characterlist.html

--- src/main/java/com/baran/charactersheetcreator/WebController.java
+++ src/main/java/com/baran/charactersheetcreator/WebController.java
@@ -20,12 +20,18 @@ public class WebController {
     }
 
     @PostMapping("/")
-    public String submitCharacterName(@ModelAttribute Character character, RedirectAttributes redirectAttrs) {
+    public String submitCharacterName(@ModelAttribute Character character, @RequestParam("name") String name, RedirectAttributes redirectAttrs) {
+        character.setName(name);
         redirectAttrs.addFlashAttribute("character", character);
-        return "redirect:/character";
+        return "redirect:/characters/" + name;
     }
-    @GetMapping("/character")
-    public String characterPage(RedirectAttributes redirectAttrs) {
+
+    @GetMapping("/characters")
+    public String showAllCharacters(){
+        return "";
+    }
+    @GetMapping("/characters/{name}")
+    public String characterListPage(RedirectAttributes redirectAttrs) {
         return "characterpage";
     }
 
