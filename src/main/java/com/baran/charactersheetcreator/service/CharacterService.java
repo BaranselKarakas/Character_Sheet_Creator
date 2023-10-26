2b0e296d63ac0f859c2e585812d3846d274c439f
BaranselKarakas <baransel@live.de>
2023-10-26 12:19:34 +0200

    add HashMap + createCharacter()

--- src/main/java/com/baran/charactersheetcreator/service/CharacterService.java
+++ src/main/java/com/baran/charactersheetcreator/service/CharacterService.java
@@ -1,11 +1,28 @@
 package com.baran.charactersheetcreator.service;
 
+import com.baran.charactersheetcreator.domain.Character;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 
 @Service
 public class CharacterService {
 
+    HashMap<String, Character> characterHashMap = new HashMap<String, Character>();
+
+    public HashMap<String, Character> getCharacterHashMap() {
+        return characterHashMap;
+    }
+
+    public void setCharacterHashMap(HashMap<String, Character> characterHashMap) {
+        this.characterHashMap = characterHashMap;
+    }
+
+    public Character createCharacter(){
+        Character character = new Character();
+        getCharacterHashMap().put(character.getName(), character);
+        return character;
     }
 
 }
