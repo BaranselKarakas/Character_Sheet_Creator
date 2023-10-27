708e9810be6667cd92cd0dc9c21b6a1888edb8ca
Baransel Karakas <karakas@synyx.de>
2023-10-27 13:00:57 +0200

    exchange HashMap with ArrayList

--- src/main/java/com/baran/charactersheetcreator/controller/WebController.java
+++ src/main/java/com/baran/charactersheetcreator/controller/WebController.java
@@ -1,6 +1,7 @@
 package com.baran.charactersheetcreator.controller;
 
 import com.baran.charactersheetcreator.domain.Character;
+import com.baran.charactersheetcreator.service.CharacterService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,8 +24,15 @@ public class WebController {
         return "charactercreator";
     }
 
+<<<<<<< HEAD
     @PostMapping("/")
     public String submitCharacterName(@ModelAttribute Character character, RedirectAttributes redirectAttrs) {
+=======
+    @PostMapping("/ ")
+    public String submitCharacterName(@ModelAttribute Character character, @RequestParam("name") String name, RedirectAttributes redirectAttrs) {
+        character.setName(name);
+        CharacterService.getArrayList().add(character);
+>>>>>>> 0137718 (exchange HashMap with ArrayList)
         redirectAttrs.addFlashAttribute("character", character);
         return "redirect:/character";
     }
--- src/main/java/com/baran/charactersheetcreator/service/CharacterService.java
+++ src/main/java/com/baran/charactersheetcreator/service/CharacterService.java
@@ -3,26 +3,22 @@ package com.baran.charactersheetcreator.service;
 import com.baran.charactersheetcreator.domain.Character;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
 @Service
 public class CharacterService {
 
-    HashMap<String, Character> characterHashMap = new HashMap<String, Character>();
+    static ArrayList<Object> arrayList = new ArrayList<>();
 
-    public HashMap<String, Character> getCharacterHashMap() {
-        return characterHashMap;
+    public static ArrayList<Object> getArrayList() {
+        return arrayList;
     }
 
-    public void setCharacterHashMap(HashMap<String, Character> characterHashMap) {
-        this.characterHashMap = characterHashMap;
+    public static void setArrayList(ArrayList<Object> arrayList) {
+        CharacterService.arrayList = arrayList;
     }
 
-    public Character createCharacter(){
-        Character character = new Character();
-        getCharacterHashMap().put(character.getName(), character);
-        return character;
-    }
 
 }
