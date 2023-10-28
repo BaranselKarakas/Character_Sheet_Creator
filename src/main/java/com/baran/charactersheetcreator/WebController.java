ccfd22eb78cc732c1ab7fde5ae0796c4a501adfc
Baransel Karakas <karakas@synyx.de>
2023-10-28 13:45:44 +0200

    adjust imports

--- src/main/java/com/baran/charactersheetcreator/WebController.java
+++ src/main/java/com/baran/charactersheetcreator/WebController.java
@@ -2,7 +2,10 @@ package com.baran.charactersheetcreator;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -24,6 +27,7 @@ public class WebController {
         redirectAttrs.addFlashAttribute("character", character);
         return "redirect:/character";
     }
+
     @GetMapping("/character")
     public String characterPage(RedirectAttributes redirectAttrs) {
         return "characterpage";
