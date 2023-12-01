9b3cdda61c0d927c71e03bcfa2dc608835b82ff0
Baransel Karakas <karakas@synyx.de>
2023-12-01 15:17:50 +0100

    add locale change interceptor

--- src/main/java/com/baran/charactersheetcreator/configuration/MessageConfig.java
+++ src/main/java/com/baran/charactersheetcreator/configuration/MessageConfig.java
@@ -3,7 +3,9 @@ package com.baran.charactersheetcreator.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
@@ -18,5 +20,17 @@ public class MessageConfig implements WebMvcConfigurer {
         slr.setLocaleAttributeName("session.current.locale");
         slr.setTimeZoneAttributeName("session.current.timezone");
         return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("language");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
