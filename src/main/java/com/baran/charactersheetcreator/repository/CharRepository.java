c305e64279e94859d103970255f3ae78ddbfa198
Baransel Karakas <karakas@synyx.de>
2024-01-05 16:08:07 +0100

    wip add persistence

--- src/main/java/com/baran/charactersheetcreator/repository/CharRepository.java
+++ src/main/java/com/baran/charactersheetcreator/repository/CharRepository.java
@@ -2,10 +2,11 @@ package com.baran.charactersheetcreator.repository;
 
 import com.baran.charactersheetcreator.domain.Character;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface CharRepository extends CrudRepository<Character, Integer> {
 
-    void delete(Optional<Character> byId);
+
 }
