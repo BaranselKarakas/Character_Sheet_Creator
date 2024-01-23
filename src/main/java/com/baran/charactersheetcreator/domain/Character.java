9d2862b1623431fc931a0f27944925f5372eb111
Baransel Karakas <karakas@synyx.de>
2024-01-23 17:12:45 +0100

    add mariadb per docker

--- src/main/java/com/baran/charactersheetcreator/domain/Character.java
+++ src/main/java/com/baran/charactersheetcreator/domain/Character.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity
+@Entity(name = "Characters")
 public class Character {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
