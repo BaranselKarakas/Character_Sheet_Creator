82a20eb197b1b771b5d1a9b9c0368131bb7904b5
Baransel Karakas <karakas@synyx.de>
2024-01-29 16:22:47 +0100

    remove unnecessary dependency

--- src/main/java/com/baran/charactersheetcreator/repository/CharRepository.java
+++ src/main/java/com/baran/charactersheetcreator/repository/CharRepository.java
@@ -3,8 +3,6 @@ package com.baran.charactersheetcreator.repository;
 import com.baran.charactersheetcreator.domain.Character;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 @Repository
 public interface CharRepository extends CrudRepository<Character, Integer> {
 
