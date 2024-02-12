a260b99a282dbe4d7d0fb1e42bec8fb1f5e54d22
Baransel Karakas <karakas@synyx.de>
2024-02-12 15:00:43 +0100

    add security configuration

---
+++ src/main/java/com/baran/charactersheetcreator/configuration/InMemoryAuthWebSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.baran.charactersheetcreator.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class InMemoryAuthWebSecurityConfiguration {
+        @Bean
+        public InMemoryUserDetailsManager userDetailsService() {
+            PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+            UserDetails admin = User.withUsername("admin")
+                    .password(encoder.encode("password"))
+                    .roles("USER")
+                    .build();
+            UserDetails user = User.withUsername("user")
+                    .password(encoder.encode("password"))
+                    .roles("USER")
+                    .build();
+            return new InMemoryUserDetailsManager(admin, user);
+        }
+    }
