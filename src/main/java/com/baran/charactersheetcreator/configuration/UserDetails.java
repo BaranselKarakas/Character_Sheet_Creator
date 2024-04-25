a36987eb6e68cafdf3555ac9f175c230d416c121
Baransel Karakas <karakas@synyx.de>
2024-04-25 16:12:54 +0200

    change pw

--- src/main/java/com/baran/charactersheetcreator/configuration/UserDetails.java
+++ src/main/java/com/baran/charactersheetcreator/configuration/UserDetails.java
@@ -13,15 +13,15 @@ public class UserDetails {
     public InMemoryUserDetailsManager userDetailsService() {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder(); // password encoder is necessary since spring security 5: https://www.baeldung.com/spring-security-5-default-password-encoder
         org.springframework.security.core.userdetails.UserDetails admin = User.withUsername("admin")
-                .password(encoder.encode("password"))
+                .password(encoder.encode("1234"))
                 .roles("ADMIN", "USER")
                 .build();
         org.springframework.security.core.userdetails.UserDetails user1 = User.withUsername("user1")
-                .password(encoder.encode("password"))
+                .password(encoder.encode("1234"))
                 .roles("USER")
                 .build();
         org.springframework.security.core.userdetails.UserDetails user2 = User.withUsername("user2")
-                .password(encoder.encode("password"))
+                .password(encoder.encode("1234"))
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(admin, user1, user2);
