3acda9eb35f4c3e726ba1f87e29eb5ad64f346e1
Baransel Karakas <baransel@live.de>
2024-09-07 20:35:47 +0200

    outsource userPassword to applicat.properties

--- src/main/java/com/baran/charactersheetcreator/configuration/UserDetails.java
+++ src/main/java/com/baran/charactersheetcreator/configuration/UserDetails.java
@@ -1,5 +1,6 @@
 package com.baran.charactersheetcreator.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
@@ -9,19 +10,23 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class UserDetails {
+
+    @Value("${user.password}")
+    private String userPassword;
+
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder(); // password encoder is necessary since spring security 5: https://www.baeldung.com/spring-security-5-default-password-encoder
         org.springframework.security.core.userdetails.UserDetails admin = User.withUsername("admin")
-                .password(encoder.encode("1234"))
+                .password(encoder.encode(userPassword))
                 .roles("ADMIN", "USER")
                 .build();
         org.springframework.security.core.userdetails.UserDetails user1 = User.withUsername("user1")
-                .password(encoder.encode("1234"))
+                .password(encoder.encode(userPassword))
                 .roles("USER")
                 .build();
         org.springframework.security.core.userdetails.UserDetails user2 = User.withUsername("user2")
-                .password(encoder.encode("1234"))
+                .password(encoder.encode(userPassword))
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(admin, user1, user2);
